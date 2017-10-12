53aea03ade79a773143a2d26e859a3f15fb7f772
Jaydee <pavel@dabel.cz>
2017-10-12 14:54:27 +0200

    NodeJS / Express / MongoDB - Build a Shopping Cart - #16 Making Charges with Stripe

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,15 @@ var Cart = require('../models/cart');
 
 /* Home Page */
 router.get('/', function(req, res, next) {
-  Product.find(function (err, docs) {
+    var successMsg = req.flash('success')[0];
+    Product.find(function (err, docs) {
       //build product array with chunkSize per row
       var productsChunks = [];
       var chunkSize = 3;
       for (var i = 0; i < docs.length; i += chunkSize) {
           productsChunks.push(docs.slice(i, i + chunkSize));
       }
-      res.render('shop/index', { title: 'Shopping Cart', products: productsChunks });
+      res.render('shop/index', { title: 'Shopping Cart', products: productsChunks, successMsg: successMsg, noMessages: !successMsg });
   });
 });
 
@@ -34,18 +35,74 @@ router.get('/add-to-cart/:id', function (req, res, next) {
 
 router.get('/shopping-cart', function (req, res, next) {
     if(!req.session.cart) {
-        res.render('shop/shopping-cart', {products: null});
+        return res.render('shop/shopping-cart', {products: null});
     }
     var cart = new Cart(req.session.cart);
     res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.priceRounded(cart.totalPrice)});
 });
 
-router.get('/checkout', function (req, res, next) {
-    if(!req.session.cart) {
+/*
+    Test credit card
+    Num:4242 4242 4242 4242
+    Exp:10/2017
+    CVC:232
+*/
+router.get('/checkout', isLoggedIn, function(req, res, next) {
+    if (!req.session.cart) {
         return res.redirect('/shopping-cart');
     }
     var cart = new Cart(req.session.cart);
-    res.render('shop/checkout', {total: cart.priceRounded(cart.totalPrice)});
+    var errMsg = req.flash('error')[0];
+    res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
 });
 
+router.post('/checkout', isLoggedIn, function(req, res, next) {
+    if (!req.session.cart) {
+        return res.redirect('/shopping-cart');
+    }
+    var cart = new Cart(req.session.cart);
+
+    var stripe = require("stripe")(
+        "sk_test_SZnMAgQevmwGEcpvtCwS1BfT"
+    );
+
+    stripe.charges.create({
+        amount: cart.priceRounded(cart.totalPrice * 100),
+        currency: "eur",
+        source: req.body.stripeToken, // obtained with Stripe.js
+        description: "Test Charge"
+    }, function(err, charge) {
+        if (err) {
+            req.flash('error', err.message);
+            return res.redirect('/checkout');
+        }
+        req.flash('success', 'Successfully bought product!');
+        req.session.cart = null;
+        res.redirect('/');
+        /*
+        var order = new Order({
+            user: req.user,
+            cart: cart,
+            address: req.body.address,
+            name: req.body.name,
+            paymentId: charge.id
+        });
+        order.save(function(err, result) {
+            req.flash('success', 'Successfully bought product!');
+            req.session.cart = null;
+            res.redirect('/');
+        });
+        */
+    });
+});
+
+
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.session.oldUrl = req.url;
+    res.redirect('/user/signin');
+}
